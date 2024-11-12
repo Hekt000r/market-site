7c752cae9926cc6241b24ba5e89d841855063377
Hektor_ <128101580+Hekt000r@users.noreply.github.com>
2024-11-12 18:53:54 +0000

    123

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import { useState } from "react";
 import { MapContainer } from "react-leaflet";
-import { TileLayer, Marker, Popup } from "react-leaflet";
+import { TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 function Map() {
+  const [location, setLocation] = useState([])
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((p) => {
+// success
+
+    }, () => {
+// error
+
+    })
+  } else{
+
+  }
   const [position, setPosition] = useState([42.00736, 20.97548]);
-// 42.006247 20.969977
-
-// TODO: ADD MARKERS FOR FOLLOWING LOCATIONS
-
-// 41.991082, 21.434602
-
-// 41.9949944, 21.3945748
-
-// 41.9954171, 21.5098008
-
-// 41.9993986, 21.4213699
-
-
   return (
     <MapContainer center={position} zoom={15.5} style={{ width: '1000px', height: '500px' }}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
